289850a09a98533499c7ee7480ef925d0e553a69
Alexis-Elaxis <videocrea.yt@gmail.com>
2022-03-02 20:59:19 +0100

    testing...

--- back.js
+++ back.js
@@ -1,39 +1,47 @@
 let cache = [];
 
+console.log(document.getElementById('list'));
+
 const fetchLinks = () => {
     fetch("https://woopy.alexiis.fr/websites.json").then(res => {
         res.json().then(j => {
             cache = j;
-            console.log("Links as been fetched !")
+            console.log("Links are been fetched !")
         })
     }).catch(err => {
-        console.log("Error : ", err);
+        console.log("Une erreur est survenue.", err);
     })
 }
 
-function checkurl(link){
+function checkurl(url){
     let is = false;
 
     for (let u of cache) {
-        if (u.link == link) is = u.redirect;
+        if (u.link == url) is = u.redirect;
     }
 
     return is;
 }
 
+
 function getURL(domain, cleared) {
     var count = cleared.length;
     var data = domain.substring(count);
 
+    if(domain.startsWith("d.s")){
+        if(data == "/woopy") {
+            return "https://discord.gg/bD5uzpBCCt";
+        }
+    }
+
     if(checkurl(cleared)) {
         return checkurl(cleared) + data;
-    } else {
-        return false;   
     }
+    return false;
 }
 
 fetchLinks();
-setInterval(fetchLinks, 60*60*1000);
+setInterval(fetchLinks, 60*60*1000)
 
 chrome.tabs.onUpdated.addListener(function(activeInfo) { //When tab is updated
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => { //We get the current URL
